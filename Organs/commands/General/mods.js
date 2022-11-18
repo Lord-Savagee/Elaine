519f9844e46b2ea45eaadeabc910a036ebfbdd2d
Lord Savage <102780931+union-savage@users.noreply.github.com>
2022-11-18 12:49:58 +0200

    Update mods.js

--- Organs/commands/General/mods.js
+++ Organs/commands/General/mods.js
@@ -6,16 +6,16 @@ module.exports={
     usage:`${prefa}mods`,
     desc:"shows mods list",
     category:"General",
-    react:"✅",
+    react:"🌺",
 
     start:async(client,m,{})=>{
         const mod= owner
         let mo=`
-*🎉 Nezuko Mods 🎉*\n`
+*🔮 Elaina Mods 🎀*\n`
 for(let i=0;i<mod.length;i++){
     const um= await client.username(mod[i]+'@s.whatsapp.net')
     mo+=`\n✨${i+1}\n*👤 Name:* ${um}\n*📱 Contact:* http://wa.me/+${mod[i].split("@")[0]}\n`
 }
-await  client.sendMessage(m.from,{image:{url:'https://images2.alphacoders.com/104/1044538.png'},caption:mo},{quoted:m})  
+await  client.sendMessage(m.from,{image:{url:'https://telegra.ph/file/8eed1568a71b3b55dac05.jpg'},caption:mo},{quoted:m})  
     }
 }
